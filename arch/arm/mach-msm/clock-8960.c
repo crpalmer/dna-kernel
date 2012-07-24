20ba593949aa7e7408a29e498ed0a00dbd06f2c0
Matt Wagantall <mattw@codeaurora.org>
2012-07-23 17:19:08 -0700

    msm: footswitch-8x60: Reset DSI domain along with MDP collapse/restore
    Some DSI-related logic lives in MDP's collapsible GDHS domain for
    8960, 8064 and 8930. Assert the reset to this domain when the reset
    of MDP is reset. Use of the reset-only clocks added here assumes the
    display driver is making sure the DSI clocks (not managed by the
    clock driver) are enabled for the duration of the collapse and
    restore sequences so that the resets propagate.
    
    Change-Id: I18638624f7ac92b9cb7228b014df7a187fe974fb
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/devices-8930.c

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -5414,8 +5414,8 @@ static struct clk_lookup msm_clocks_8064[] = {
 	CLK_LOOKUP("mem_clk",		ebi1_acpu_a_clk.c, ""),
 	CLK_LOOKUP("bus_clk",		afab_acpu_a_clk.c, ""),
 
-	CLK_LOOKUP("reset1_clk",	dsi1_reset_clk.c, ""),
-	CLK_LOOKUP("reset2_clk",	dsi2_reset_clk.c, ""),
+	CLK_LOOKUP("reset1_clk",	dsi1_reset_clk.c, "footswitch-8x60.4"),
+	CLK_LOOKUP("reset2_clk",	dsi2_reset_clk.c, "footswitch-8x60.4"),
 
 	CLK_LOOKUP("l2_mclk",		l2_m_clk,     ""),
 	CLK_LOOKUP("krait0_mclk",	krait0_m_clk, ""),
@@ -5776,8 +5776,8 @@ static struct clk_lookup msm_clocks_8960_common[] = {
 	CLK_LOOKUP("mem_clk",		ebi1_acpu_a_clk.c, ""),
 	CLK_LOOKUP("bus_clk",		afab_acpu_a_clk.c, ""),
 
-	CLK_LOOKUP("reset1_clk",	dsi1_reset_clk.c, ""),
-	CLK_LOOKUP("reset2_clk",	dsi2_reset_clk.c, ""),
+	CLK_LOOKUP("reset1_clk",	dsi1_reset_clk.c, "footswitch-8x60.4"),
+	CLK_LOOKUP("reset2_clk",	dsi2_reset_clk.c, "footswitch-8x60.4"),
 
 	CLK_LOOKUP("l2_mclk",		l2_m_clk,     ""),
 	CLK_LOOKUP("krait0_mclk",	krait0_m_clk, ""),
@@ -6529,7 +6529,7 @@ static struct clk_lookup msm_clocks_8930[] = {
 	CLK_LOOKUP("mem_clk",		ebi1_acpu_a_clk.c, ""),
 	CLK_LOOKUP("bus_clk",		afab_acpu_a_clk.c, ""),
 
-	CLK_LOOKUP("reset1_clk",	dsi1_reset_clk.c, ""),
+	CLK_LOOKUP("reset1_clk",	dsi1_reset_clk.c, "footswitch-8x60.4"),
 
 	CLK_LOOKUP("l2_mclk",		l2_m_clk,     ""),
 	CLK_LOOKUP("krait0_mclk",	krait0_m_clk, ""),
