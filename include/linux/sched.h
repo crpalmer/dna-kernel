bb618e91ffed7b2a98ea8073719f2c6c60eae3cd
Paul Turner <pjt@google.com>
2012-10-05 19:53:51 -0700

    sched: track the runnable average on a per-task entity basis
    Instead of tracking averaging the load parented by a cfs_rq, we can track
    entity load directly.  With the load for a given cfs_rq then being the sum of
    its children.
    
    To do this we represent the historical contribution to runnable average within
    each trailing 1024us of execution as the coefficients of a geometric series.
    
    We can express this for a given task t as:
      runnable_sum(t) = \Sum u_i * y^i, runnable_avg_period(t) = \Sum 1024 * y^i
      load(t) = weight_t * runnable_sum(t) / runnable_avg_period(t)
    
    Where: u_i is the usage in the last i`th 1024us period (approximately 1ms) ~ms
    and y is chosen such that y^k = 1/2.  We currently choose k to be 32 which
    roughly translates to about a sched period.
    
    Signed-off-by: Paul Turner <pjt@google.com>
    Reviewed-by: Ben Segall <bsegall@google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -913,6 +913,16 @@ struct load_weight {
 	unsigned long weight, inv_weight;
 };
 
+struct sched_avg {
+	/*
+	 * These sums represent an infinite geometric series and so are bound
+	 * above by 1024/(1-y).  Thus we only need a u32 to store them for for all
+	 * choices of y < 1-2^(-32)*1024.
+	 */
+	u32 runnable_avg_sum, runnable_avg_period;
+	u64 last_runnable_update;
+};
+
 #ifdef CONFIG_SCHEDSTATS
 struct sched_statistics {
 	u64			wait_start;
@@ -972,6 +982,9 @@ struct sched_entity {
 	struct cfs_rq		*cfs_rq;
 	
 	struct cfs_rq		*my_q;
+#endif
+#ifdef CONFIG_SMP
+	struct sched_avg	avg;
 #endif
 };
 
