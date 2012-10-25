7134b80267200029553d0f82fac34db39e2f95f0
Kuogee Hsieh <khsieh@codeaurora.org>
2012-10-25 09:07:58 -0700

    msm_fb: display: fix iommu leak during base layer swap
    Retain RGB1 iommu information during  base layer swap
    so that iommu used by RGB1 can be freed back to pool later.
    
    Change-Id: Ibed85179284743dc8a9d1434f0c81626b0567e16
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    (cherry picked from commit be46f9f596c5172438a74759d48ca7db600cd737)

--- drivers/video/msm/mdp4_overlay.c
+++ drivers/video/msm/mdp4_overlay.c
@@ -1689,6 +1689,7 @@ void mdp4_overlay_borderfill_stage_up(struct mdp4_overlay_pipe *pipe)
 	struct mdp4_overlay_pipe *bspipe;
 	int ptype, pnum, pndx, mixer;
 	int format, alpha_enable, alpha;
+	struct mdp4_iommu_pipe_info iom;
 
 	if (pipe->pipe_type != OVERLAY_TYPE_BF)
 		return;
@@ -1765,6 +1766,7 @@ void mdp4_overlay_borderfill_stage_down(struct mdp4_overlay_pipe *pipe)
 		return;
 	}
 
+	iom = bspipe->iommu;
 	ptype = bspipe->pipe_type;
 	pnum = bspipe->pipe_num;
 	pndx = bspipe->pipe_ndx;
@@ -1778,6 +1780,7 @@ void mdp4_overlay_borderfill_stage_down(struct mdp4_overlay_pipe *pipe)
 	bspipe->src_format = format;
 	bspipe->alpha_enable = alpha_enable;
 	bspipe->alpha = alpha;
+	bspipe->iommu = iom;
 
 	bspipe->pipe_used++;	/* mark base layer pipe used */
 
