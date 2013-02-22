9f18a81c670348bbeb5c57be78b354727765f09d
Christopher R. Palmer <crpalmer@gmail.com>
2013-02-21 19:55:35 -0500

    Fix Linaro 4.8 detected problem in the bluetooth code
    It only oopies 4 or 8 bytes of the 16 bytes it should have
    copied because the people who wrote this code don't understand
    what an array means in a function declaration.

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -224,7 +224,8 @@ void hci_le_start_enc(struct hci_conn *conn, __le16 ediv, __u8 rand[8],
 }
 EXPORT_SYMBOL(hci_le_start_enc);
 
-void hci_le_ltk_reply(struct hci_conn *conn, u8 ltk[16])
+#define LTK_SIZE	16
+void hci_le_ltk_reply(struct hci_conn *conn, u8 ltk[LTK_SIZE])
 {
 	struct hci_dev *hdev = conn->hdev;
 	struct hci_cp_le_ltk_reply cp;
@@ -234,7 +235,7 @@ void hci_le_ltk_reply(struct hci_conn *conn, u8 ltk[16])
 	memset(&cp, 0, sizeof(cp));
 
 	cp.handle = cpu_to_le16(conn->handle);
-	memcpy(cp.ltk, ltk, sizeof(ltk));
+	memcpy(cp.ltk, ltk, LTK_SIZE);
 
 	hci_send_cmd(hdev, HCI_OP_LE_LTK_REPLY, sizeof(cp), &cp);
 }
