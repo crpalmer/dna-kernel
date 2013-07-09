2c3f6fa26c3ecb26991e8f667cad60a8b3ae7ac5
Christopher R. Palmer <crpalmer@gmail.com>
2013-07-09 12:14:56 -0400

    Sync with caf's board-8064-gpu.c
    Note that this changes the second GPU frequency from 325000000
    to 320000000.  This is important because the Fmax table has entries
    for 400000000 and 320000000 and the frequencies are matched against
    the Fmax table.
    
    Prior to this commit the actual frequencies were 400000000 for the
    first two scaling levels and 325000000 was never possible.

--- arch/arm/mach-msm/board-monarudo-gpu.c
+++ arch/arm/mach-msm/board-monarudo-gpu.c
@@ -1,5 +1,4 @@
-/* Copyright (c) 2012, Code Aurora Forum. All rights reserved.
- * Copyright (c) 2012, LGE Inc.
+/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -30,7 +29,6 @@ static struct msm_dcvs_freq_entry grp3d_freq[] = {
 	{0, 950, 0, 0, 0},
 	{0, 1200, 1, 100, 100},
 };
-
 
 static struct msm_dcvs_core_info grp3d_core_info = {
 	.freq_tbl		= &grp3d_freq[0],
@@ -95,13 +93,13 @@ static struct msm_bus_vectors grp3d_low_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1500),
+		.ib = KGSL_CONVERT_TO_MBPS(1000),
 	},
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_3D_PORT1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1500),
+		.ib = KGSL_CONVERT_TO_MBPS(1000),
 	},
 };
 
@@ -228,7 +226,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
-			.gpu_freq = 325000000,
+			.gpu_freq = 320000000,
 			.bus_freq = 3,
 			.io_fraction = 33,
 		},
@@ -278,11 +276,17 @@ void __init apq8064_init_gpu(void)
 {
 	unsigned int version = socinfo_get_version();
 
-	if ((SOCINFO_VERSION_MAJOR(version) == 1) &&
-			(SOCINFO_VERSION_MINOR(version) == 1))
-		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 1);
-	else
-		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 0);
+	if (cpu_is_apq8064ab())
+		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 450000000;
+	if (SOCINFO_VERSION_MAJOR(version) == 2) {
+		kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 2);
+	} else {
+		if ((SOCINFO_VERSION_MAJOR(version) == 1) &&
+				(SOCINFO_VERSION_MINOR(version) == 1))
+			kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 1);
+		else
+			kgsl_3d0_pdata.chipid = ADRENO_CHIPID(3, 2, 0, 0);
+	}
 
 	platform_device_register(&device_kgsl_3d0);
 }
