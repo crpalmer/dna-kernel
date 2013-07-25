34fee5786a956812b69223cf07e99cfdb5918a0d
Christopher R. Palmer <crpalmer@gmail.com>
2013-07-25 19:35:59 -0400

    monarudo-display: change min pwm for backlight
    The m7 kernel has some special code for backlights below 21.
    To try to fix flickering issues, bump our min up to 21 so that
    none of the special code (if it were required) would matter.
    
    I don't think that this is really necessary, so we can try
    reverting it later.

--- arch/arm/mach-msm/board-monarudo-display.c
+++ arch/arm/mach-msm/board-monarudo-display.c
@@ -708,7 +708,7 @@ static void monarudo_display_on(struct msm_fb_data_type *mfd)
 	PR_DISP_INFO("%s\n", __func__);
 }
 
-#define PWM_MIN                   13
+#define PWM_MIN                   21
 #define PWM_DEFAULT               82
 #define PWM_MAX                   255
 
