be6f905d73fd47746e73ee5e939cb043dfb15518
Christopher R. Palmer <crpalmer@gmail.com>
2013-09-08 07:15:58 -0400

    dlx: display: Enable splash screen continuation

--- arch/arm/mach-msm/board-monarudo-display.c
+++ arch/arm/mach-msm/board-monarudo-display.c
@@ -545,7 +545,7 @@ static struct msm_panel_common_pdata mdp_pdata = {
 #else
 	.mem_hid = MEMTYPE_EBI1,
 #endif
-	.cont_splash_enabled = 0x00,
+	.cont_splash_enabled = 0x01,
 	.mdp_iommu_split_domain = 1,
 };
 
