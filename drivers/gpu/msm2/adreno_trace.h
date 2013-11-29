f5dbf2ce7a9182219f17bf90decc1dc8c37cec94
Christopher R. Palmer <crpalmer@gmail.com>
2013-11-28 19:12:05 -0500

    Revert "hack up adreno_trace to work with new kernel builds"
    This reverts commit 15e082e43150f837584dd19d8396a352b0475fc0.

--- drivers/gpu/msm2/adreno_trace.h
+++ drivers/gpu/msm2/adreno_trace.h
@@ -17,7 +17,7 @@
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM kgsl
 #undef TRACE_INCLUDE_PATH
-#define TRACE_INCLUDE_PATH ../../drivers/gpu/msm2
+#define TRACE_INCLUDE_PATH .
 #define TRACE_INCLUDE_FILE adreno_trace
 
 #include <linux/tracepoint.h>
