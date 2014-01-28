efe742af7efb7942a03d23e3ca4be28fba128337
Jamie Morris <jamiethemorris@gmail.com>
2014-01-27 18:10:07 -0800

    Implement kexec-hardboot
    M7 kexec-hardboot patch, courtesy of Tasssadar
    
    Conflicts:
    	arch/arm/configs/jamiethemorris_defconfig

--- arch/arm/mach-msm/board-monarudo.c
+++ arch/arm/mach-msm/board-monarudo.c
@@ -4864,6 +4864,17 @@ static void __init monarudo_common_init(void)
 
 static void __init monarudo_allocate_memory_regions(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page at the end of first system ram block
+	struct membank* bank = &meminfo.bank[0];
+	phys_addr_t start = bank->start + bank->size - SZ_1M;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
 	monarudo_allocate_fb_region();
 }
 
