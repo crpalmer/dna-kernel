cb430a5d0d15de999bb10d7e7693e37fa30bec8c
Christopher R. Palmer <crpalmer@gmail.com>
2014-01-30 16:55:02 -0500

    Revert "[DO NOT COMMIT]: pull the gpio_input driver from the M7 kernel"
    This reverts commit 786f5a9906fc8909753b25f29c05d575157a9fd3.

--- include/linux/gpio_event.h
+++ include/linux/gpio_event.h
@@ -18,10 +18,6 @@
 
 #include <linux/input.h>
 
-#define KEY_LOGD(fmt, args...) printk(KERN_DEBUG "[KEY] "fmt, ##args)
-#define KEY_LOGI(fmt, args...) printk(KERN_INFO "[KEY] "fmt, ##args)
-#define KEY_LOGE(fmt, args...) printk(KERN_ERR "[KEY] "fmt, ##args)
-
 struct gpio_event_input_devs {
 	int count;
 	struct input_dev *dev[];
@@ -39,9 +35,8 @@ struct gpio_event_info {
 	int (*event)(struct gpio_event_input_devs *input_devs,
 		     struct gpio_event_info *info,
 		     void **data, unsigned int dev, unsigned int type,
-		     unsigned int code, int value); 
+		     unsigned int code, int value); /* out events */
 	bool no_suspend;
-	uint8_t rrm1_mode;
 };
 
 struct gpio_event_platform_data {
@@ -49,16 +44,16 @@ struct gpio_event_platform_data {
 	struct gpio_event_info **info;
 	size_t info_count;
 	int (*power)(const struct gpio_event_platform_data *pdata, bool on);
-	uint8_t cmcc_disable_reset;
-	const char *names[]; 
-			     
+	const char *names[]; /* If name is NULL, names contain a NULL */
+			     /* terminated list of input devices to create */
 };
 
 #define GPIO_EVENT_DEV_NAME "gpio-event"
 
+/* Key matrix */
 
 enum gpio_event_matrix_flags {
-	
+	/* unset: drive active output low, set: drive active output high */
 	GPIOKPF_ACTIVE_HIGH              = 1U << 0,
 	GPIOKPF_DEBOUNCE                 = 1U << 1,
 	GPIOKPF_REMOVE_SOME_PHANTOM_KEYS = 1U << 2,
@@ -79,25 +74,28 @@ enum gpio_event_matrix_flags {
 extern int gpio_event_matrix_func(struct gpio_event_input_devs *input_devs,
 			struct gpio_event_info *info, void **data, int func);
 struct gpio_event_matrix_info {
-	
+	/* initialize to gpio_event_matrix_func */
 	struct gpio_event_info info;
-	
+	/* size must be ninputs * noutputs */
 	const unsigned short *keymap;
 	unsigned int *input_gpios;
 	unsigned int *output_gpios;
 	unsigned int ninputs;
 	unsigned int noutputs;
-	
-	struct timespec settle_time;
-	
-	struct timespec debounce_delay;
-	struct timespec poll_time;
+	/* time to wait before reading inputs after driving each output */
+	ktime_t settle_time;
+	/* time to wait before scanning the keypad a second time */
+	ktime_t debounce_delay;
+	ktime_t poll_time;
 	unsigned flags;
 };
 
+/* Directly connected inputs and outputs */
 
 enum gpio_event_direct_flags {
 	GPIOEDF_ACTIVE_HIGH         = 1U << 0,
+/*	GPIOEDF_USE_DOWN_IRQ        = 1U << 1, */
+/*	GPIOEDF_USE_IRQ             = (1U << 2) | GPIOIDF_USE_DOWN_IRQ, */
 	GPIOEDF_PRINT_KEYS          = 1U << 8,
 	GPIOEDF_PRINT_KEY_DEBOUNCE  = 1U << 9,
 	GPIOEDF_PRINT_KEY_UNSTABLE  = 1U << 10,
@@ -107,13 +105,13 @@ struct gpio_event_direct_entry {
 	uint32_t gpio:16;
 	uint32_t code:10;
 	uint32_t dev:6;
-	bool     not_wakeup_src;
 };
 
+/* inputs */
 extern int gpio_event_input_func(struct gpio_event_input_devs *input_devs,
 			struct gpio_event_info *info, void **data, int func);
 struct gpio_event_input_info {
-	
+	/* initialize to gpio_event_input_func */
 	struct gpio_event_info info;
 	ktime_t debounce_time;
 	ktime_t poll_time;
@@ -121,11 +119,9 @@ struct gpio_event_input_info {
 	uint16_t type;
 	const struct gpio_event_direct_entry *keymap;
 	size_t keymap_size;
-	void (*setup_input_gpio)(void);
-	void (*set_qty_irq)(uint8_t);
-	void (*clear_hw_reset)(void);
 };
 
+/* outputs */
 extern int gpio_event_output_func(struct gpio_event_input_devs *input_devs,
 			struct gpio_event_info *info, void **data, int func);
 extern int gpio_event_output_event(struct gpio_event_input_devs *input_devs,
@@ -133,7 +129,7 @@ extern int gpio_event_output_event(struct gpio_event_input_devs *input_devs,
 			unsigned int dev, unsigned int type,
 			unsigned int code, int value);
 struct gpio_event_output_info {
-	
+	/* initialize to gpio_event_output_func and gpio_event_output_event */
 	struct gpio_event_info info;
 	uint16_t flags;
 	uint16_t type;
@@ -142,6 +138,7 @@ struct gpio_event_output_info {
 };
 
 
+/* axes */
 
 enum gpio_event_axis_flags {
 	GPIOEAF_PRINT_UNKNOWN_DIRECTION  = 1U << 16,
@@ -152,11 +149,11 @@ enum gpio_event_axis_flags {
 extern int gpio_event_axis_func(struct gpio_event_input_devs *input_devs,
 			struct gpio_event_info *info, void **data, int func);
 struct gpio_event_axis_info {
-	
+	/* initialize to gpio_event_axis_func */
 	struct gpio_event_info info;
-	uint8_t  count; 
-	uint8_t  dev; 
-	uint8_t  type; 
+	uint8_t  count; /* number of gpios for this axis */
+	uint8_t  dev; /* device index when using multiple input devices */
+	uint8_t  type; /* EV_REL or EV_ABS */
 	uint16_t code;
 	uint16_t decoded_size;
 	uint16_t (*map)(struct gpio_event_axis_info *info, uint16_t in);
@@ -170,18 +167,4 @@ uint16_t gpio_axis_4bit_gray_map(
 uint16_t gpio_axis_5bit_singletrack_map(
 			struct gpio_event_axis_info *info, uint16_t in);
 
-extern int gpio_event_switch_func(struct gpio_event_input_devs *input_devs,
-			struct gpio_event_info *info, void **data, int func);
-struct gpio_event_switch_info {
-	
-	struct gpio_event_info info;
-	ktime_t debounce_time;
-	ktime_t poll_time;
-	uint16_t flags;
-	uint16_t type;
-	const struct gpio_event_direct_entry *keymap;
-	size_t keymap_size;
-	void (*setup_switch_gpio)(void);
-	void (*set_qty_irq)(uint8_t);
-};
 #endif
